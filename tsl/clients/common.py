e9bb15da4ed166522d77817186c02658e7386bc3
Mikalai Kukhta <mikalai.kukhta@epidemicsound.com>
2024-12-16 13:55:36 +0100

    include package version in User-Agent

--- tsl/clients/common.py
+++ tsl/clients/common.py
@@ -1,6 +1,9 @@
+from functools import cached_property
 from typing import Any, Dict, List, NamedTuple, Tuple, Union
 
 import aiohttp
+
+from .. import __version__
 
 
 class UrlParams(NamedTuple):
@@ -12,11 +15,18 @@ class AsyncClient:
     def __init__(self, session: aiohttp.ClientSession) -> None:
         self._session = session
 
+    @cached_property
+    def user_agent(self):
+        return f"{aiohttp.http.SERVER_SOFTWARE} trafiklab-sl/{__version__}"
+
     async def _request_json(self, args: UrlParams):
         response = await self._session.get(
             args.url,
             params=args.params,
-            headers={"Content-Type": "application/json"},
+            headers={
+                "Content-Type": "application/json",
+                "User-Agent": self.user_agent,
+            },
         )
         response.raise_for_status()
         json = await response.json()
