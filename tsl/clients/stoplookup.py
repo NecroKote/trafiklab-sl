15d303a0e744285b2013adf3aa1891dc40133638
delize <4028612+delize@users.noreply.github.com>
2025-12-06 18:57:59 +0100

    Fix deprecated import for Python 3.12 compatibility

--- tsl/clients/stoplookup.py
+++ tsl/clients/stoplookup.py
@@ -1,5 +1,9 @@
 from typing import List
-from warnings import deprecated
+
+try:
+    from warnings import deprecated
+except ImportError:
+    from typing_extensions import deprecated
 
 from ..models.journey import SearchLeg, StopFilter
 from ..models.stops import StopFinderType
